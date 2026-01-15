007960a717caad66f60b9f05aa40df4701dda89a
Yarden <jorden.a17@gmail.com>
2026-01-15 23:18:33 +0200

    Add delay sending to Kafka

--- news-ingestion-service/src/main/java/com/sentiment/newsingestion/client/NewsApiClient.java
+++ news-ingestion-service/src/main/java/com/sentiment/newsingestion/client/NewsApiClient.java
@@ -3,7 +3,16 @@ package com.sentiment.newsingestion.client;
 import com.sentiment.newsingestion.model.NewsStream;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class NewsApiClient {
@@ -25,6 +34,7 @@ public class NewsApiClient {
                         .build())
                 .retrieve()
                 .bodyToMono(NewsStream.NewsApiResponse.class)
-                .flatMapIterable(NewsStream.NewsApiResponse::getArticlesLines);
+                .flatMapIterable(NewsStream.NewsApiResponse::getArticlesLines)
+                .delayElements(Duration.ofMillis(300));
     }
 }
