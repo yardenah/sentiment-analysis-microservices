9e235761cf6e09a37ee43be1906adc23236c4d8f
Yarden <jorden.a17@gmail.com>
2026-03-25 12:03:25 +0200

    Add sentiment analyzer

---
+++ sentiment-analysis-service/src/main/java/com/sentiment/sentimentanalysis/nlp/SentimentAnalyzer.java
@@ -0,0 +1,38 @@
+package com.sentiment.sentimentanalysis.nlp;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Service
+public class SentimentAnalyzer {
+    private final StanfordCoreNLP nlp;
+    private final Map<String, Double> sentimentValues = new HashMap<>();
+
+    public SentimentAnalyzer() {
+        Properties p = new Properties();
+        p.put("annotators", "tokenize, ssplit, parse, sentiment");
+        this.nlp = new StanfordCoreNLP(p);
+
+        sentimentValues.put("Very negative", 1d);
+        sentimentValues.put("Negative", 2d);
+        sentimentValues.put("Neutral", 3d);
+        sentimentValues.put("Positive", 4d);
+        sentimentValues.put("Very positive", 5d);
+    }
+
+    public double analyze(String text) {
+        var ann = nlp.process(text);
+        var sentences = ann.get(CoreAnnotations.SentencesAnnotation.class);
+        return sentences.stream()
+                .map(s -> s.get(SentimentCoreAnnotations.SentimentClass.class))
+                .mapToDouble(sentimentValues::get)
+                .average()
+                .orElse(0.0);
+    }
+}
+
